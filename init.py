e6e68951755e3320f3da55ed3ad8c0b79dcbe35d
2758Cobalt <kolomoets551@gmail.com>
2023-03-30 12:27:53 +0300

    created init,mysql_client files,  upgrade main file

---
+++ init.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import tkinter.messagebox as messagebox
+from tkinter import ttk
+from os import  system as sys
+#from mysql import
+
+
+#packages
+sys('cls')
+
+
+# constains
+app_title = "app"
+app_resolution = "512x256"
+
+"""
+TOP (по умолчанию, выравнивается по верхней стороне контейнера), 
+BOTTOM (выравнивание по нижней стороне), 
+LEFT (выравнивание по левой стороне), 
+RIGHT (выравнивание по правой стороне).
+"""
+
+"""
+Noth(север - вверх), 
+South (юг - низ), 
+East (восток - правая сторона), 
+West (запад - левая сторона),
+Center (по центру). Например, значение nw указывает на верхний левый угол
+
+NW  N   NE
+W   C   E
+SW  S   SE
+
+C-center
+"""
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-from tkinter import *
-from tkinter import ttk
+from init import *
+
+root = Tk()
+root.title(app_title)
+root.geometry(app_resolution)
+
+def get_text():
+    print(editor.get()) # test
+
+def button(label="text",command=None,side=TOP,anchor=NW): # test
+    return ttk.Button(text=label,command=command).pack(side=side,anchor=anchor)
+
+def label(label="text",side=TOP,anchor=NW):# test
+    return ttk.Label(text=label).pack(side=side,anchor=anchor)
+
+
+editor = Entry()
+editor.pack()
+
+button("click",command=get_text,side=BOTTOM,anchor=CENTER)
+
+root.mainloop()
